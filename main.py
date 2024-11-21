358cee222eb2a7228ed07da69c8c1c30d1f51f23
CuriousTony <aszdorovtsev@yandex.ru>
2024-11-21 14:32:08 +0300

    Added basic main.py methods.

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template, request
 import requests
 
-
 app = Flask(__name__)
 
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather = None
+    if request.method == 'POST':
+        city = request.form['city']
+        weather = get_weather(city)
+    return render_template('index.html', weather=weather)
+
+
+def get_weather(city):
+    api_key = '42eb404b0e6e802f09643d87eba6593a'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    response = requests.get(url)
+    return response.json()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
